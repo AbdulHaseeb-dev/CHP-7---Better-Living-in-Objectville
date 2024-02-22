3b4a2c2de2071bc0350065cd648e3d76cad83ab6
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-02-22 10:26:14 +0500

    different classes made

--- Rowboat.java
+++ Rowboat.java
@@ -3,15 +3,3 @@ public class Rowboat extends Boat {
         System.out.println("stroke natasha");
     }
 }
-public class Boat {
-   private int length ;
-   public void setLength (int len) {
-    length = len;
-   }
-    public int getLength() {
-        return length ;
-    }
-    public void move() {
-        System.out.println("drift");
-    }
-}
---
+++ Sailboat.java
@@ -0,0 +1,8 @@
+public class Sailboat extends Boat {
+        public void move() {
+          System.out.println("hoist sail");
+  
+        }
+      
+  }
+
